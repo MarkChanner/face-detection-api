58979aecc997d171a84fd270b96d77bc46b1532f
Mark Channer <markchanner@gmail.com>
2019-12-18 14:37:06 +0000

    Dynamically assign port number and add home route

--- server.js
+++ server.js
@@ -24,6 +24,10 @@ const app = express();
 app.use(cors());
 app.use(bodyParser.json());
 
+app.get('/', (req, res) => {
+  res.send('It is working..');
+});
+
 app.post('/register', (req, res) => {
   register.handleRegister(req, res, bcrypt, db);
 });
@@ -44,6 +48,7 @@ app.post('/imageurl', (req, res) => {
   image.handleApiCall(req, res);
 });
 
-app.listen(3000, () => {
-  console.log('Listening on Port 3000');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on Port ${PORT}`);
 });
